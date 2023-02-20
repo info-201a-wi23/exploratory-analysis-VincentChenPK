336b918847f11b623d0e0aafa6595629f9d8427d
Lily Bui <lilybui327@gmail.com>
2023-02-20 15:11:15 -0800

    fixed formatting

--- chart2.R
+++ chart2.R
@@ -1,5 +1,4 @@
-read.csv("companies_1m.csv", stringsAsFactors = F)
-company_data <- read.csv("companies_1m.csv", stringsAsFactors = F)
+company_data <- read.csv("data_set/companies_1m.csv", stringsAsFactors = F)
 library("ggplot2")
 library("plotly")
 library("dplyr")
@@ -13,10 +12,3 @@ lat_neg_90 <- company_data %>% filter(latitude > -90 & latitude <= -45)
 # Taking size of each Group as a measure of how many offices are there
 office_lat_data <- c(nrow(lat_90), nrow(lat_45), nrow(lat_neg_45), nrow(lat_neg_90))
 
-# Plotting the Pie Chart
-pie(office_lat_data, 
-    labels = c("90 - 45", "45 - 0", "0 - -45", "-45 -90"),
-    radius = 1.0,
-    main = "Number of Tech Offices in Each Latitude Group",
-    col = palette("Set2")
-)
